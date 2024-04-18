39dad907204cfcf32df4421b11ec214cfc3b5e4c
Vishalrajeshthakor <167396418+Vishalrajeshthakor@users.noreply.github.com>
2024-04-18 11:52:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,36 +1,35 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @Filethumbnailbot
+# Subscribe Instagram page For Amazing Bot @silent_jethalal_back
+# Ask Doubt on telegram @movie4hollywood
 
 
 import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("28416899", "")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("18216e288ed0676fc986abc1526c1358", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("7030179376:AAHsOLxufrDUslwe0T5uTztChV2FBikrTgs", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "silent_jethalal_back") 
 
-             # Don't Remove Credit @VJ_Botz
-             # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-             # Ask Doubt on telegram @KingVJ01
+             # Don't Remove Credit @Filethumbnailbot
+             # Subscribe YouTube Channel For Amazing Bot @Filethumbnailbot
+             # Ask Doubt on telegram @movie4hollywood
+DB_NAME = os.environ.get("DB_NAME", "renamefilethumbnailbot")     
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
-
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("mongodb+srv://kajalagarwal12334321:FQeRWyY1aK7c5qJR@cluster0.pjkymxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7030179376').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit  @Filethumbnailbot
+# Subscribe Instagram page For Amazing Bot @silent_jethalal_back
+# Ask Doubt on telegram @movie4hollywood
